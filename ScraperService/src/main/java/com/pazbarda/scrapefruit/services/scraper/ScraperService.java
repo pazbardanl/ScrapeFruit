03bf919b0cf18ee9c42d33d48ebff03c43f63e24
Paz Barda <paz.barda@intel.com>
2024-09-24 13:13:08 +0300

    skeleton ScraperService gradle project

---
+++ ScraperService/src/main/java/com/pazbarda/scrapefruit/services/scraper/ScraperService.java
@@ -0,0 +1,7 @@
+package com.pazbarda.scrapefruit.services.scraper;
+
+public class ScraperService {
+    public static void main(String[] args) {
+        System.out.println("ScraperService - Hello world!");
+    }
+}
