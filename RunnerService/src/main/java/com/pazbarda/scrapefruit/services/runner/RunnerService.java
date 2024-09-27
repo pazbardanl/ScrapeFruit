ba28d3a307d1e9e05d512641f03976b8037f2f07
Paz Barda <paz.barda@intel.com>
2024-09-27 23:30:37 +0300

    skeleton RunnerService

---
+++ RunnerService/src/main/java/com/pazbarda/scrapefruit/services/runner/RunnerService.java
@@ -0,0 +1,7 @@
+package com.pazbarda.scrapefruit.services.runner;
+
+public class RunnerService {
+    public static void main(String[] args) {
+        System.out.println("RunnerService - Hello world!");
+    }
+}
