2e0daf4ac24578f2c7d5a667d8ba2d7860a7f6f5
Paz Barda <paz.barda@intel.com>
2024-09-28 19:27:19 +0300

    jsoup-scraping POC (bbc.com)

--- ScraperService/src/main/java/com/pazbarda/scrapefruit/services/scraper/ScraperService.java
+++ ScraperService/src/main/java/com/pazbarda/scrapefruit/services/scraper/ScraperService.java
@@ -1,7 +1,30 @@
 package com.pazbarda.scrapefruit.services.scraper;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class ScraperService {
     public static void main(String[] args) {
         System.out.println("ScraperService - Hello world!");
+        try {
+            scrapeUrl("https://www.bbc.com/");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String scrapeUrl(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        String htmlContent = doc.html();
+        String plainTextContent = doc.text();
+
+        System.out.println("HTML Content: ");
+        System.out.println(htmlContent);
+
+        System.out.println("\nPlain Text Content: ");
+        System.out.println(plainTextContent);
+        return null;
     }
 }
